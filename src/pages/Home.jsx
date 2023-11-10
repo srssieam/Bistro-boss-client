431cae14f011633721412c1c758c6fe390069355
srs-sieam <srssieam@gmail.com>
2023-11-10 08:05:40 +0600

    installed react-router-dom and tailwind css and created router and layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center text-3xl text-green-700">Hello sieam</h1>
+        </div>
+    );
+};
+
+export default Home;
