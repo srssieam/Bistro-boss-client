212efc3ae2de0cbf91129fe0dd9e13d34573b8ca
srs-sieam <srssieam@gmail.com>
2023-11-10 08:23:21 +0600

    installed daisyui and created a footer

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../SharedComponentes/Footer";
 
 
 const Root = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
