dd40a8d3dd70914b937d4bdeb5e5220fd3b76f10
srs-sieam <srssieam@gmail.com>
2023-11-10 15:26:19 +0600

    installed swiper js and created cagetory section

---
+++ src/SharedComponentes/Category.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+
+import slide1 from '../assets/home/slide1.jpg'
+import slide2 from '../assets/home/slide2.jpg'
+import slide3 from '../assets/home/slide3.jpg'
+import slide4 from '../assets/home/slide4.jpg'
+import slide5 from '../assets/home/slide5.jpg'
+
+const Category = () => {
+  return (
+    <div className='max-w-7xl mx-auto px-6 lg:px-0'>
+      <Swiper className=" mySwiper mb-9"
+        slidesPerView={3}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 15
+          }
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide><img className='w-full h-full object-cover' src={slide1} alt="" /><h3 className='text-4xl text-center -mt-20 uppercase drop-shadow-md text-white'>Salad</h3></SwiperSlide>
+        <SwiperSlide><img className='w-full h-full object-cover' src={slide2} alt="" /><h3 className='text-4xl text-center -mt-20 uppercase drop-shadow-md text-white'>Pizza</h3></SwiperSlide>
+        <SwiperSlide><img className='w-full h-full object-cover' src={slide3} alt="" /><h3 className='text-4xl text-center -mt-20 uppercase drop-shadow-md text-white'>Soup</h3></SwiperSlide>
+        <SwiperSlide><img className='w-full h-full object-cover' src={slide4} alt="" /><h3 className='text-4xl text-center -mt-20 uppercase drop-shadow-md text-white'>desserts</h3></SwiperSlide>
+        <SwiperSlide><img className='w-full h-full object-cover' src={slide5} alt="" /><h3 className='text-4xl text-center -mt-20 uppercase drop-shadow-md text-white'>Drinks</h3></SwiperSlide>
+
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../SharedComponentes/Banner";
+import Category from "../SharedComponentes/Category";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
