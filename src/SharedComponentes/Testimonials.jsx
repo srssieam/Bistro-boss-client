9e922fb86fcce0f95cd62e5a397607876561eb29
srs-sieam <srssieam@gmail.com>
2023-11-12 07:57:55 +0600

    created pagination with swiper js

--- src/SharedComponentes/Testimonials.jsx
+++ src/SharedComponentes/Testimonials.jsx
@@ -11,7 +11,7 @@ import qoute from '../assets/quote-left 1.png'
 const Testimonials = () => {
     const [reviews, setReviews] = useState([])
     useEffect(() => {
-        fetch('reviews.json')
+        fetch('http://localhost:5000/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
--- src/pages/our shop/ShopCategoryItems.jsx
+++ src/pages/our shop/ShopCategoryItems.jsx
@@ -1,13 +1,46 @@
 import FoodCard from "../../SharedComponentes/FoodCard";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './Shop.css'
 
 
-const ShopCategoryItems = ({items}) => {
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+
+const ShopCategoryItems = ({ items }) => {
+    console.log(items.length)
+    const totalPage = Math.ceil(items.length / 6);
+    const pages = [...Array(totalPage).keys()];
+    console.log(pages)
+    console.log(totalPage)
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    };
     return (
-        <div className="grid md:grid-cols-2 gap-8 lg:grid-cols-3 px-6 lg:px-0 my-10">
-                        {
-                            items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
-                        }
+
+        <Swiper
+            pagination={pagination}
+            modules={[Pagination]}
+            className="mySwiper"
+        >
+            {pages.map((page) => (
+                <SwiperSlide key={page}>
+                    <div className="grid md:grid-cols-2 gap-8 lg:grid-cols-3 px-6 lg:px-0 my-10">
+                        {items.slice(page * 6, (page + 1) * 6).map((item) => (
+                            <FoodCard key={item._id} item={item}></FoodCard>
+                        ))}
                     </div>
+                </SwiperSlide>
+            ))}
+
+        </Swiper>
     );
 };
 
