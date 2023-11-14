21f4d7dd5dbfe7ebdeb6bc8c5f6b961ffe27e9c7
srs-sieam <srssieam@gmail.com>
2023-11-14 07:47:18 +0600

    Enforce user to login before add to the cart

--- src/SharedComponentes/FoodCard.jsx
+++ src/SharedComponentes/FoodCard.jsx
@@ -1,7 +1,34 @@
+import Swal from "sweetalert2";
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 
 const FoodCard = ({ item }) => {
     const { name, image, recipe, price } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const handleAddToCart = (food)=>{
+        console.log(food, user?.email)
+        if(user && user.email){
+            // Todo: send cart item to the database
+        }
+        else{
+            Swal.fire({
+                title: "Login first",
+                text: "Please login to add to the cart!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    // send the user to the login page
+                    navigate('/login')
+                }
+              });
+        }
+    }
     return (
         <div className="shadow-xl">
             <div className="max-h-72 relative">
@@ -12,7 +39,7 @@ const FoodCard = ({ item }) => {
                 <h1 className="text-2xl font-semibold">{name}</h1>
                 <p>{recipe}</p>
                 <div className="flex justify-center my-8">
-                    <button className="btn bg-transparent md:text-xl font-semibold border-0 border-b-4 text-yellow-600 border-b-yellow-700  shadow-xl">add to cart</button>
+                    <button onClick={()=>handleAddToCart(item)} className="btn bg-transparent md:text-xl font-semibold border-0 border-b-4 text-yellow-600 border-b-yellow-700  shadow-xl">add to cart</button>
                 </div>
             </div>
         </div>
