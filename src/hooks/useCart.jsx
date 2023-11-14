a718c92e170e25843363a4b25cdbb7dd60d4d01c
srs-sieam <srssieam@gmail.com>
2023-11-14 09:50:18 +0600

    get cart items for different user email using query parameter

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
-
+import useAuth from "./useAuth"
 
 const useCart = () => {
     const axiosSecure = useAxiosSecure()
+    const { user } = useAuth();
+
     const {data: cart = []} = useQuery({
-        queryKey: ['cart'],
+        queryKey: ['cart', user?.email], // create different key for different user
         queryFn: async ()=>{
-            const res = await axiosSecure.get('/carts')
+            const res = await axiosSecure.get(`/carts?email=${user.email}`)
             return res.data
         }
     })
