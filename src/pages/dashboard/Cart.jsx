435a64c5614480f358ecd03912cc3525c5a101a7
srs-sieam <srssieam@gmail.com>
2023-11-14 12:04:03 +0600

    created cart list

--- src/pages/dashboard/Cart.jsx
+++ src/pages/dashboard/Cart.jsx
@@ -1,10 +1,10 @@
 import SectionTitle from "../../SharedComponentes/SectionTitle";
 import useCart from "../../hooks/useCart";
-
+import { RiDeleteBin6Line } from 'react-icons/ri';
 
 const Cart = () => {
     const [cart] = useCart();
-    const totalPrice = cart.reduce( (total, item) => total + item.price, 0)
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
     return (
         <div className="px-24">
             <SectionTitle subHeading={"My Cart"} heading={"Wanna add more?"}></SectionTitle>
@@ -16,7 +16,44 @@ const Cart = () => {
 
             {/* item list */}
             <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr className="text-xl font-semibold">
+                                <th></th>
+                                <th>Item Image</th>
+                                <th>Item Name</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                cart?.map((item, idx) => <tr key={item._id}>
+                                    <td>{idx + 1}</td>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-14 h-14">
+                                                <img src={item.image} />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {item.name}
+                                    </td>
+                                    <td>${item.price}</td>
+                                    <th>
+                                        <button className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
+                                    </th>
+                                </tr>)
+                            }
 
+                        </tbody>
+
+                    </table>
+                </div>
             </div>
         </div>
     );
