f425fcc5729d5fba57b3022559581ea59b9d02c7
srs-sieam <srssieam@gmail.com>
2023-11-16 09:50:16 +0600

    delete user: send user id to the server and delete from database.then refetch data

--- src/pages/dashboardAdmin/ManageUsers.jsx
+++ src/pages/dashboardAdmin/ManageUsers.jsx
@@ -3,17 +3,51 @@ import SectionTitle from "../../SharedComponentes/SectionTitle";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { FaUsers } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
     const axiosSecure = useAxiosSecure();
 
-    const { data: users = [] } = useQuery({
+    const {refetch, data: users = [] } = useQuery({
         queryKey: ['users'],
         queryFn: async () =>{
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleDeleteUser = user =>{
+        console.log("user to delete",user)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {    
+            axiosSecure.delete(`/users/${user._id}`)
+                .then(res =>{
+                    // console.log(res);
+                    refetch()
+                    if(res.data.deletedCount > 0){
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "User has been deleted.",
+                            icon: "success"
+                          });
+                    }
+                })
+            }
+          });
+    }
+
+    
+    const handleUpdateUser = user =>{
+        console.log("user to update",user)
+    }
 
     return (
         <div className="px-24">
@@ -41,10 +75,10 @@ const ManageUsers = () => {
                                     </td>
                                     <td>${user.email}</td>
                                     <th>
-                                        <button className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-[#e69040]"><FaUsers></FaUsers></button>
+                                        <button onClick={()=>handleUpdateUser(user)} className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-[#e69040]"><FaUsers></FaUsers></button>
                                     </th>
                                     <th>
-                                        <button className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
+                                        <button onClick={()=>handleDeleteUser(user)} className="p-2 rounded-md hover:bg-red-500 text-2xl text-white bg-red-700"><RiDeleteBin6Line></RiDeleteBin6Line></button>
                                     </th>
                                 </tr>)
                             }
