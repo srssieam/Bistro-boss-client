c0fcfbf34b596db276b8ed5522996504a6da1a6d
srs-sieam <srssieam@gmail.com>
2023-11-16 11:46:28 +0600

    sent current user email to the server and receive a token from server and store the token in localStorage

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
@@ -8,6 +9,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const GoogleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -39,6 +41,20 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+            const userInfo = { email: currentUser?.email };
+            if(currentUser){
+                // send email and get token then store token in localStorage
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if(res.data.token){
+                            localStorage.setItem('access-token', res.data.token)
+                        }
+                    })
+            }
+            else{
+                // remove token from localStorage (if there is no current user)
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         })
         return () => {
