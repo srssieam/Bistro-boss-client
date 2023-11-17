31f9239cd71baa13903aa85b685eddde76117f60
srs-sieam <srssieam@gmail.com>
2023-11-17 08:55:35 +0600

    Upload image to image hosting server imgbb and get image url

--- src/pages/dashboardAdmin/AddItems.jsx
+++ src/pages/dashboardAdmin/AddItems.jsx
@@ -1,12 +1,24 @@
 import { FaUtensils } from "react-icons/fa6";
 import SectionTitle from "../../SharedComponentes/SectionTitle";
 import { useForm } from "react-hook-form"
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
-
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddItems = () => {
     const { register, handleSubmit} = useForm();
-    const onSubmit = data => {
+    const axiosPublic = useAxiosPublic();
+
+    const onSubmit = async data => {
         console.log(data)
+        const imageFile = { image: data.image[0] }
+        // image upload to imgbb and then get an url
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        console.log(res.data)
     }
     return (
         <div className="px-24">
